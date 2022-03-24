b81ad73a90358e23e7521a08396c73058260140a
PriyadiSentosa <priyadisentosa825@gmail.com>
2022-03-24 17:51:54 +0700

    heroku

--- routes/api.php
+++ routes/api.php
@@ -19,4 +19,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('supplier', [ApiController::class, 'index']);
+Route::get('suplier2', [ApiController::class, 'index']);
