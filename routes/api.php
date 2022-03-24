31a4a7f55603b9df18f0ac6e2465a327be2c8cd3
PriyadiSentosa <priyadisentosa825@gmail.com>
2022-03-24 17:28:45 +0700

    heroku

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\API\ApiController;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route; 
+use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,4 +21,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('suplier', [ApiController::class, 'suplier']);
+Route::get('suplier2', [ApiController::class, 'suplier']);
